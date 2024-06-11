74b4f05e8563283cc9d4a0e6565587a6536280e4
migueelmartinezzz <maartinez.miguel476@gmail.com>
2024-06-11 11:12:35 -0300

    Update trabalho.last.py

--- trabalho.last.py
+++ trabalho.last.py
@@ -36,7 +36,7 @@ def plot_popularity(similar_artists):
 
 def main():
     st.title("HPIYFA")
-    st.title('How :red[Popular] Is Your Favorite Artist?')
+    st.header('How :red[Popular] Is Your Favorite Artist?')
     st.subheader('O site que te mostra o quão popular seu artista favorito é', divider='rainbow')
     api_key = '7df4a32d7aec2f6a2fbe9cd02c3a5a6e'
     artist_name = st.text_input("Digite o nome do seu artista favorito:")
