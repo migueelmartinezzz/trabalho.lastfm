d5ac0329457b32b1a36a969b51c28652cbc9afbc
migueelmartinezzz <maartinez.miguel476@gmail.com>
2024-06-11 21:46:27 -0300

    Update trabalho.last.py

--- trabalho.last.py
+++ trabalho.last.py
@@ -46,7 +46,7 @@ def main():
     """
     <style>
     .stApp {
-        background: url('https://github.com/migueelmartinezzz/trabalho.lastfm/blob/main/back%20stage.jpg?raw=true') no-repeat center center fixed;
+        background: url('https://github.com/migueelmartinezzz/trabalho.lastfm/blob/main/fundo%20site%20miguel.jpg?raw=true') no-repeat center center fixed;
         background-size: cover;
     }
     </style>
