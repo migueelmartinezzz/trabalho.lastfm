27e75f3debff36d867f9ae45989dcb78169e2b66
migueelmartinezzz <maartinez.miguel476@gmail.com>
2024-06-17 12:22:11 -0300

    Update trabalho.last.py

--- trabalho.last.py
+++ trabalho.last.py
@@ -4,32 +4,21 @@ import matplotlib.pyplot as plt
 from collections import Counter
 
 def get_similar_artists(artist_name, api_key):
-    url_similar = f'http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist={artist_name}&api_key={api_key}&format=json'
-    response_similar = requests.get(url_similar)
-    
-    if response_similar.status_code == 200:
-        data = response_similar.json()
+    url = f'http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist={artist_name}&api_key={api_key}&format=json'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
         similar_artists = Counter()
-        
         for artist in data['similarartists']['artist']:
-            artist_name = artist['name']  # Renomeie para artist_name para simplificar
-            url_info = f'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist={artist_name}&api_key={api_key}&format=json'
-            response_info = requests.get(url_info)
-            
-            if response_info.status_code == 200:
-                data_info = response_info.json()
-                
-                if 'artist' in data_info:
-                    listeners = data_info['artist']['stats']['listeners']
-                    similar_artists[artist_name] = listeners
-                else:
-                    st.warning(f"Não foi possível obter informações para o artista {artist_name}.")
-            else:
-                st.warning(f"Erro ao consultar a API para o artista {artist_name}: {response_info.status_code}")
-        
+            novo_nome = artist['name']
+            url = f'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist={novo_nome}&api_key={api_key}&format=json'
+            response = requests.get(url)
+            data2 = response.json()
+            if 'artist' in data2:
+                similar_artists[novo_nome] = data2['artist']['stats']['listeners']
         return similar_artists
     else:
-        st.error("Erro ao consultar a API: {}".format(response_similar.status_code))
+        st.error("Erro ao consultar a API: {}".format(response.status_code))
         return None
 
 def plot_popularity(similar_artists):
@@ -70,9 +59,8 @@ def main():
             st.write(f"Artistas semelhantes a '{artist_name}':")
             for artist, popularity in similar_artists.most_common(10):
                 st.write(f"{artist}: {popularity} ouvintes")
-            plot_popularity(similar_artists.most_common(10))
+            plot_popularity(similar_artists)
         else:
             st.warning("Não foi possível obter artistas semelhantes ao seu favorito.")
-
 if __name__ == "__main__":
     main()
